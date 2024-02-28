a608b29a1a45bd091050737e5d3373d3a6def458
Lyokakz <lenusik__rybka@mail.ru>
2024-02-28 23:35:55 +0600

    Add Lesson03:Task02

---
+++ Lesson03/Task02/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Задача №2
+Вывод на экран квадратов чисел от 1 до N.
+*/
+
+
+
+int n = 7;
+int[] arr = new int[n];
+FillArray(arr);
+PrintArray(arr);
+int sum = GetSumOfElements(arr);
+int product = GetProductOfElements(arr);
+Console.WriteLine("Сумма элементов массива: " + sum);
+Console.WriteLine("Произведение элементов массива: " + product);
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = i + 1;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Элементы массива:");
+    foreach (int element in arr)
+    {
+        Console.Write(element + " ");
+    }
+    Console.WriteLine();
+}
+
+int GetSumOfElements(int[] arr)
+{
+    int sum = 0;
+    foreach (int element in arr)
+    {
+        sum++;
+    }
+    return sum;
+}
+
+int GetProductOfElements(int[] arr)
+{
+    int product = 1;
+    foreach (int element in arr)
+    {
+        product = product * element;
+    }
+    return product;
+}
+
+
+
+
+
