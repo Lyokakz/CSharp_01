bec44c2cdf5faa7244da2da8d70ba20681be44f8
Lyokakz <lenusik__rybka@mail.ru>
2024-02-28 22:54:38 +0600

    Add Lesson03:Task01

--- Lesson03/Task01/Program.cs
+++ Lesson03/Task01/Program.cs
@@ -1,4 +1,10 @@
-﻿double CalculateFormula(int a, int b, int c, int d)
+﻿/*
+Задача №1
+Вычислить значение функции a*b/c+d, где
+a, b, c, d некоторые числа. Решение оформить в виде функции.
+*/
+
+double CalculateFormula(int a, int b, int c, int d)
 {
     double numenator = a * b;
     int denomenator = c + d;
