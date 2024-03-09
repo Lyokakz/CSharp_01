a0fba492c2c6d3c98c5e67454da0211bb9e4b753
Lyokakz <lenusik__rybka@mail.ru>
2024-03-09 23:39:11 +0500

    Add Lesson05:Task01

---
+++ Lesson05/Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача №1
+• Вычислить факториал от натурального
+числа N
+*/
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop requrson: n = {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    int result = n * Fact(n - 1);
+    Console.WriteLine($"Возврат: n = {n}, fact = {result}");
+
+    return result;
+}
+
+
+Console.Write(Fact(5));
+
+// F11 шаг с заходом
+// F10 шаг с обходом
+// F5 продолжить до следующей точки останова
